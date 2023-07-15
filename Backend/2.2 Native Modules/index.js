4053edc11e2d56a89faacedce3b366fc1dcd29f9
baohng <bao.hng221b@gmail.com>
2023-07-15 15:37:01 +0700

    module 10: Node.js - native modules

---
+++ Backend/2.2 Native Modules/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// fs.writeFile("own-test.txt", "Hello from baohng!", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// })
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data)
+})
