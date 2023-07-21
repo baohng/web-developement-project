325fa0ecab0c1eac68e4fa649e649e354e43c84c
baohng <bao.hng221b@gmail.com>
2023-07-21 22:31:09 +0700

    exercise 1 to get familiar to JavaScript

---
+++ Eloquent JavaScript/triangle.js
@@ -0,0 +1,8 @@
+(function triangle() {
+  const triangleWidth = 7;
+  let signal = "";
+  for (let item = 1; item <= triangleWidth; item++) {
+    console.log(signal+=`#`);
+  }
+})();
+console.log(`-----------------------`);
