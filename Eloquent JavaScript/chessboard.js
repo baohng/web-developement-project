d56a819787d19792a1b7a94bb8cd7e08424d2686
baohng <bao.hng221b@gmail.com>
2023-07-22 09:44:36 +0700

    chessboard exercise - optimize

--- Eloquent JavaScript/chessboard.js
+++ Eloquent JavaScript/chessboard.js
@@ -1,26 +1,14 @@
 (function chessboard() {
-   const width = 8, height = 8;
-   for (let iter = 1; iter <= height; iter++) {
-    if ((iter + 2) % 2 != 0) {
-      let hashtag = ` `;
-      for (let iter2 = 1; iter2 <= width; iter2++) {
-        if ((iter2 + 2) % 2 == 0) {
-          hashtag += `#`;
-        } else {
-          hashtag += ` `;
-        }
+  const width = 8, height = 8;
+  for (let row = 1; row <= height; row++) {
+    let hashtag = ``;
+    for (let col = 1; col <= width; col++) {
+      if ((row + col) % 2 == 0) {
+        hashtag += ` `;
+      } else {
+        hashtag += `#`;
       }
-      console.log(hashtag);
-    } else {
-      let hashtag = ` `;
-      for (let iter2 = 1; iter2 <= width; iter2++) {
-        if ((iter2 + 2) % 2 == 0) {
-          hashtag += ` `;
-        } else {
-          hashtag += `#`;
-        }
-      }
-      console.log(hashtag);
     }
-   }
+    console.log(hashtag);
+  }
 })();
