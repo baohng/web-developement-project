5cd9472fa4c2772d1cf4f5cf27840d55a7737080
baohng <bao.hng221b@gmail.com>
2023-07-27 10:15:29 +0700

    complete flow as expected

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -6,7 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-
+let brandName = "";
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -15,9 +15,17 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
 });
 
+function brandNameGenerate(req, res, next) {
+  console.log(req.body);
+  brandName = req.body["street"] + req.body["pet"];
+  next();
+};
+
+app.use(brandNameGenerate);
+
 app.post("/submit", (req, res) => {
-  res.send(`Your brand name is \n ${req.body["street"] + req.body["pet"]}`);
-})
+  res.send(`Your brand name is \n ${brandName}`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
