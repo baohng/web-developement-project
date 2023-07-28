ba68bf32bb6e57c5815097538ba858db348ff1e6
baohng <bao.hng221b@gmail.com>
2023-07-28 10:59:04 +0700

    practicing passing data from client to server and reverse

---
+++ Backend/4.2 Passing Data/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  let nameLength = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs",{
+    length : nameLength,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
