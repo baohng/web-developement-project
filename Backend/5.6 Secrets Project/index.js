516f570899b6e2386402d5e065de3856309df51c
baohng <bao.hng221b@gmail.com>
2023-07-31 10:15:29 +0700

    review kAPI knowledge

---
+++ Backend/5.6 Secrets Project/index.js
@@ -0,0 +1,37 @@
+// HINTS:
+// 1. Import express and axios
+import express from 'express';
+import axios from 'axios';
+
+// 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
+
+// 3. Use the public folder for static files.
+app.use(express.static("public"));
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+const API_URL = "https://secrets-api.appbrewery.com/";
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(
+      API_URL + `random`
+    );
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    res.render("index.ejs", {
+      secret: error.response.data
+    })
+  }
+});
+  
+
+// 6. Listen on your predefined port and start the server.
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port},`);
+});
